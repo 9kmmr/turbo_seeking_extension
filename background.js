c4c34545ad5daffde9dbb5845d4b80eaba29429c
lamvu <tunglam195@gmail.com>
2019-04-23 15:27:19 +0700

    first work on extension side

--- background.js
+++ background.js
@@ -1,35 +1,347 @@
-uid, email, access_token;
+
+siteurl = "http://localhost/turbo_seeking/";
+
+var uid, email, access_token, old_url;
+maxTries = 3;
 chrome.storage.sync.get(['uid','email','access_token'], function(items){
-    uid = items.uid;
-    email = items.email;
-    access_token = items.access_token;
+    if (items) {
+        uid = items.uid;
+        email = items.email;
+        access_token = items.access_token;
+        console.log(uid, email, access_token)
+    }
+    chrome.storage.sync.get('cannedmessage', function(item){
+        if (!item) {
+            chrome.storage.sync.set({'cannedmessage':'{"message1":"Hey","message2":"Hey","message3":"Hey","message41":"Hey","message5":"Hey"}'});
+        }
+    })
+    chrome.storage.sync.get('delaymode', function(item){
+        if (!item) {
+            chrome.storage.sync.set({'delaymode':0});
+        }
+    })
+
 })
 
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.getidentity) {
         chrome.storage.sync.get(['uid','email','access_token'], function(items){
             uid = items.uid;
             email = items.email;
-            access_token = items.access_token;
+            access_token = items.access_token;            
             if (uid&&email&&access_token) {
-                sendResponse(items)
+                sendResponse(true)
             } else 
                 sendResponse(null)
 
         })
+        return true;
     }
-    if (request.saveidentity) {
-         
-    }
-    if (request.updateidentity) {
-         
-    }
-    if (request.savecannedmessages) {
-         
-    }
-    if (request.savecannedmessages) {
-         
+    if (request.getsettings) {
+        chrome.storage.sync.get('delaymode', function(res) {
+            sendResponse(res.delaymode);
+        })
+        return true;
     }
 
+    if (request.state) {
+        user = JSON.parse(request.detail);
+        access_token = user.authToken.token
+        uid = user.user.user_uid;
+        email = user.user.email;
+        
+        chrome.storage.sync.get('access_token', function(item) {
+            if (!item.access_token) {
+                chrome.storage.sync.set({'access_token': access_token});
+            }
+        })
+        chrome.storage.sync.get('uid', function(item) {
+            if (!item.uid) {
+                chrome.storage.sync.set({'uid': uid});
+            }
+        })
+        chrome.storage.sync.get('email', function(item) {
+            if (!item.email) {
+                chrome.storage.sync.set({'email': email});
+            }
+        })  
+        return true;    
+       
+    }
+    if (request.savecannedmessages) {
+        if (request.cannedmessage) {
+            // save to storage
+            chrome.storage.sync.set({'cannedmessage':request.cannedmessage});
+            // save to server
+        }
+        return true;
+    }
+    if (request.getcannedmessages) {
+        chrome.storage.sync.get('cannedmessage',function(item) {
+            sendResponse(item.cannedmessage)
+        });
+        return true;
+    }
+
+    if (request.startsearch) {
+        
+        _theUrl = "https://api.seeking.com/v3/users/"+uid+"/search?"+request.startsearch.split("search?")[1]
+        console.log(_theUrl)
+        if (old_url!=_theUrl) {
+            requestGetLive(_theUrl, 5).then(function(response) {
+                console.log(response)
+                if (response) {
+                    combineProfileData(response.response.profiles.data, function(returned_users) {
+                        combineUserPhotos(returned_users).then(users_data =>{
+                            console.log(users_data)
+                            sendResponse({"currentpage":response.response.profiles.current_page,"next_page":response.response.profiles.next_page_url,"profiles":users_data});        
+                        })
+                        
+                    })
+                    
+                } else 
+                    sendResponse(null)
+            });
+            old_url=_theUrl;
+        }
+        return true;
+    }
+    return true;
+
 });
+
+const requestGetLive = (url,  n) => {
+    options={
+        method:"GET",
+        headers: {
+            "Authorization": "Bearer "+access_token
+        }
+    }
+    fetchedData = fetch(url, options).then(
+       function(response) {
+            status = response.status;
+            if (status==200)
+                return response.json();
+            if (status==403) {
+                if (n === 1) {
+                    return null
+                };
+                requestGetAccessToken(function(){})
+                return requestGetLive(url,  n - 1);
+            } else {
+                if (n === 1) {
+                    return null
+                };
+                requestGetAccessToken(function(){})
+                return requestGetLive(url,  n - 1);
+            }
+            
+        }
+    ).catch(function(error) {
+        if (n === 1) {           
+            return null;
+        };
+        return requestGetLive(url, n - 1);
+    });
+    return fetchedData;
+}
+
+function combineProfileData(data, callback) {    
+    users = [];
+    
+    if (data) {
+        data.forEach((element, index) => {          
+            users.push ({
+                            'name' : element.profile_attributes.username[0].value || 'Seeking Username',
+                            'age' :element.age || '',
+                            'location' :element.primary_location.name || '',
+                            'height' :element.height || '', 
+                            'body' :element.profile_attributes.body_type[0].value || '',
+                            'ethnicity' :element.profile_attributes.ethnicity[0].value || '',
+                            'favorited': element.favorited_by[0]||0,
+                            'user_uid': element.uid
+                            //'photos' :photos 
+            }); 
+            /*  */
+        });
+    }
+    callback(users);
+}
+async function combineUserPhotos(datauser) {
+    newusers = [];
+    if (datauser) {
+        for (const element of datauser) {            
+            element.photos = await requestGetProfile("https://api.seeking.com/v3/users/"+uid+"/views/"+element.user_uid+"?with=photos,isUserReportedAlready,isMemberHasPrivatePhotoPermission,memberNote&lang=en_US", 5).then(user_profile => {       
+                if (user_profile.status=="OK") {
+                    return user_profile.response.profile;                                                 
+                }                 
+            }).then(us=>{
+                photos = [us.profile_pic.thumb];
+                us.photos.public.approved.forEach(element => {
+                    photos.push(element.url.thumb);
+                });
+                return photos;
+            }) ;
+              
+            newusers.push(element)                   
+        }
+    }
+    return await Promise.all(newusers);
+    
+}
+
+async function requestGetProfile(url, n) {
+    options={
+        method:"GET",
+        headers: {
+            "Authorization": "Bearer "+access_token
+        }
+    }
+    fetchedData =  await fetch(url, options).then(
+       function(response) {
+            status = response.status;
+            if (status==200) {
+                return response.json();
+            }
+            if (status==403) {
+                if (n === 1) {
+                    return null
+                };
+                requestGetAccessToken(function(){})
+                return requestGetProfile(url,  n - 1);
+            } else {
+                if (n === 1) {
+                    return null
+                };
+                requestGetAccessToken(function(){})
+                return requestGetProfile(url,  n - 1);
+            }
+            
+        }
+    ).catch(function(error) {
+        if (n === 1) {           
+            return null;
+        };
+        return requestGetProfile(url, n - 1);
+    });
+    return fetchedData;
+}
+
+
+
+function requestFavorite(f_uid, callback) {
+    
+    _theUrl = "https://api.seeking.com/v3/users/"+uid+"/favorites/"+f_uid
+    try {        
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+            else if (xmlHttp.readyState == 4 && xmlHttp.status == 430) {
+                requestGetAccessToken(function(){})
+                console.log('403')
+                callback('fail')
+            }
+            else 
+                callback(null);
+    
+        }
+        xmlHttp.open("POST", _theUrl, true); // true for asynchronous 
+        xmlHttp.setRequestHeader("Authorization", "Bearer "+access_token); 
+        xmlHttp.setRequestHeader("Accept", "application/json");    
+        xmlHttp.setRequestHeader("Content-Type", "application/json");    
+        xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");      
+        let data = JSON.stringify({});
+        xmlHttp.send(data);
+    } catch (error) {
+        console.log('error can not favorite');
+    }
+}
+function requestSendMessage(s_uid, message, callback) {
+    
+    _theUrl = "https://api.seeking.com/v3/users/"+uid+"/conversations"
+    try {        
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+            else if (xmlHttp.readyState == 4 && xmlHttp.status == 430) {
+                console.log('403')
+                
+                callback('fail')
+            }
+            else 
+                callback(null);    
+        }
+        xmlHttp.open("POST", _theUrl, true); // true for asynchronous 
+        xmlHttp.setRequestHeader("Authorization", "Bearer "+access_token);    
+        xmlHttp.setRequestHeader("Accept", "application/json");    
+        xmlHttp.setRequestHeader("Content-Type", "application/json");    
+        xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");   
+          
+        let data = JSON.stringify({"body":message, "member_uid":s_uid});
+        xmlHttp.send(data);
+    } catch (error) {
+        console.log('error can not favorite');
+    }
+}
+
+function requestGetAccessToken(callback){
+    _theUrl = "https://api.seeking.com/v3/users/"+uid+"/auth/token"
+    try {        
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+                if (xmlHttp.responseText){
+                    data = JSON.parse(xmlHttp.responseText)
+                    if (data.status=="OK") {
+                        access_token = data.response;
+                        chrome.storage.sync.set({'access_token': access_token});
+                        callback();
+                    }
+                }
+            }
+            else if (xmlHttp.readyState == 4 && xmlHttp.status == 430) {
+                console.log('403')
+                callback('false')
+            }
+            else 
+                callback(null);    
+        }
+        xmlHttp.open("PUT", _theUrl, true); // true for asynchronous 
+        xmlHttp.setRequestHeader("Authorization", "Bearer "+access_token);    
+        xmlHttp.setRequestHeader("Accept", "application/json");    
+        xmlHttp.setRequestHeader("Content-Type", "application/json");    
+        xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");   
+          
+        let data = JSON.stringify({});
+        xmlHttp.send(data);
+    } catch (error) {
+        console.log('error can not favorite');
+    }
+}
+
+function httpGetAsync(_theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", _theUrl, true); // true for asynchronous 
+    xmlHttp.send();
+}
+
+function httpPostAsync(_theUrl, _data, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("POST", _theUrl, true); // true for asynchronous 
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    let data = JSON.stringify(_data)
+    xhr.send(data);
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,51 +1,120 @@
 
+var currentpage, page_index, next_page,m;
 
-
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.linksLeft) {
-            uid = window.localStorage.getItem('uid');
-            email = window.localStorage.getItem('email');
-            access_token = window.localStorage.getItem('access_token');
-        }
-    }
-);
-
-/* chrome.extension.sendMessage({ getidentity: 1 }, function (response) {
+ chrome.extension.sendMessage({ getidentity: 1 }, function (response) {
     if (response) {
-        uid = response.uid;
-        email = response.email;
-        access_token = response.access_token;
-        document.getElementById("authenticated").style.display = "block";
+        chrome.extension.sendMessage({ getsettings: 1 }, function (response) {
+            if (response) {
+                document.getElementById('delaymode').setAttribute('checked',true);
+            }
+        })
+        chrome.extension.sendMessage({ getcannedmessages: 1 }, function (response) {
+            if (response) {
+                console.log(response);
+            }
 
+        })
+
+
+        document.getElementById("authenticated").style.display = "block";
 
     } else {
         document.getElementById("not-auth").style.display = "block";
     }
-}) */
+})
 
-
-function httpGetAsync(_theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+searchbtn = document.getElementById('start-search');
+searchbtn.onclick = function(e){
+    e.preventDefault();
+    $('#result-search-section').empty();
+    searchurl = document.getElementById('search-url').value ;
+    if (searchurl) {
+        
+        dosearch(searchurl);
     }
-    xmlHttp.open("GET", _theUrl, true); // true for asynchronous 
-    xmlHttp.send();
 }
 
-function httpPostAsync(_theUrl, _data, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("POST", _theUrl, true); // true for asynchronous 
-    xmlHttp.setRequestHeader("Content-Type", "application/json");
-    let data = JSON.stringify(_data)
-    xhr.send(data);
+function dosearch(url) {
+    $('#result-search-section').append("<p id='ajax-loader' style='color:red;' >Loading...</p>")
+    chrome.extension.sendMessage({ startsearch: url }, function (response) {
+        
+        if (response) {
+            currentpage = response.currentpage;
+            next_page = response.next_page;
+            page_index = currentpage;
+            console.log(response)
+            showProfiles(response.profiles, function(){
+                $('#ajax-loader').remove();
+                
+            })
+        } 
+        return true;           
+    })
+    return false;
 }
+
+function showProfiles(data, callback) {
+    data.forEach(element => {
+        combinePhoto(element.photos,  photo=> {
+            console.log(photo)
+            tmp = '<div class="profiles" data-uid="'+element.user_uid+'">'   
+                +'<div id="carousel-'+element.user_uid+'" class="carousel slide" data-ride="carousel" data-pause="false">'
+                    +'<div class="carousel-inner">'
+                        +(photo)
+                    +'</div>'
+                    
+                    +(element.photos.length>1?'<a class="right carousel-control" href="#carousel-'+element.user_uid+'" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>':'')
+                +'</div>'      
+                
+                +'<div class="button-functions">'                    
+                        +'<input class="toggle-heart-check" type="checkbox" id="cb_'+element.user_uid+'"  '+(element.favorited?"checked":"")+' />'
+                        +'<label class="toggle-heart" for="cb_'+element.user_uid+'" data-toggle="tooltip" data-placement="top" title="'+(element.favorited?"UnFavorite":"Favorite")+'">❤</label>'
+                    
+                        +'<button id="canned-btn-send-1" class="canned-btn-send-mes" data-toggle="tooltip" data-placement="top" title="Hooray!">#1</button>'
+                        +'<button id="canned-btn-send-2" class="canned-btn-send-mes" data-toggle="tooltip" data-placement="top" title="Hooray!">#2</button>'
+                        +'<button id="canned-btn-send-3" class="canned-btn-send-mes" data-toggle="tooltip" data-placement="top" title="Hooray!">#3</button>'
+                        +'<button id="canned-btn-send-4" class="canned-btn-send-mes" data-toggle="tooltip" data-placement="top" title="Hooray!">#4</button>'
+                        +'<button id="canned-btn-send-5" class="canned-btn-send-mes" data-toggle="tooltip" data-placement="top" title="Hooray!">#5</button>'
+
+                +'</div>'
+                +'<div class="user-details">'
+                    +'<span class="user-address">'+element.location+'</span>'
+                    +'-'
+                    +'<span class="user-age">'+element.age+'</span>'
+                    +'-'
+                    +'<span class="user-body">'+element.body+'</span>'
+                    +'-'
+                    +'<span class="user-height">'+element.height+'</span>'
+                +'</div>'                
+            +'</div>';
+
+            $('#result-search-section').append(tmp);
+        })
+        
+    });
+    callback();
+}
+function combinePhoto(photos, callback) {
+    html = "";
+    if (photos)
+    photos.forEach((element,index) => {
+        html += '<div class="item '+(index==0?"active":"")+'">'
+                +'<img src="'+element+'">'
+                +'</div>';
+    });
+    callback(html)
+}
+// sceamless scroll
+result_search_section = document.getElementById('result-search-section');
+result_search_section.addEventListener('scroll', function(event) { 
+    event.preventDefault();   
+    if(event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight){
+        if (next_page) {
+            if (page_index!=next_page){
+                dosearch(next_page);
+                page_index=next_page;
+            }
+        }
+    }
+})
+
